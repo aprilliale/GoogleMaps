7a0bc2102674e8d3b8cf4599f4c5922d1028a619
eunhyukee44 <aprillia.le@gmail.com>
2017-10-14 15:38:57 +0700

    lima Lesson

--- app/src/main/java/id/sch/smktelkom_mlg/learning/map/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learning/map/MainActivity.java
@@ -1,47 +1,28 @@
 package id.sch.smktelkom_mlg.learning.map;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
 
-import static id.sch.smktelkom_mlg.learning.map.R.id.map;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends FragmentActivity implements OnStreetViewPanoramaReadyCallback {
 
-    static final CameraPosition SEATTLE = CameraPosition.builder()
-            .target(new LatLng(47.6204, -122.2491)).zoom(10)
-            .bearing(0).tilt(45)
-            .build();
-    GoogleMap m_map;
-    boolean mapReady = false;
 
-    LatLng renton = new LatLng(47.489805, -122.120502);
-    LatLng kirkland = new LatLng(47.7301986, -122.1768858);
-    LatLng everett = new LatLng(47.978748, -122.202001);
-    LatLng lynnwood = new LatLng(47.819533, -122.32288);
-    LatLng montlake = new LatLng(47.7973733, -122.3281771);
-    LatLng kent = new LatLng(47.385938, -122.258212);
-    LatLng showare = new LatLng(47.38702, -122.23986);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(map);
-        mapFragment.getMapAsync(this);
-
+        StreetViewPanoramaFragment streetViewPanoramaFragment =
+                (StreetViewPanoramaFragment) getFragmentManager()
+                        .findFragmentById(R.id.streetviewpanorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
 //        Button btnMap = (Button) findViewById(R.id.btnMap);
 //        btnMap.setOnClickListener(new View.OnClickListener() {
 //
@@ -74,45 +55,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(SEATTLE));
-        //map.addPolyline(new PolylineOptions().geodesic(true).add(renton).add(kirkland).add(everett).add(lynnwood).add(montlake).add(kent).add(showare).add(renton));
-        //map.addPolygon(new PolygonOptions().add(renton, kirkland, everett, lynnwood).fillColor(Color.GREEN));
-        googleMap.addCircle(new CircleOptions()
-                .center(renton)
-                .radius(5000)
-                .strokeColor(Color.GREEN)
-                .fillColor(Color.argb(64, 0, 255, 0)));
-//        mapReady = true;
-//        m_map = googleMap;
-//        LatLng eaciit = new LatLng(-7.284790, 112.736989);
-//        CameraPosition target = CameraPosition.builder().target(eaciit).zoom(14).build();
-//        m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void flyTo(CameraPosition target) {
-        m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+    public void onStreetViewPanoramaReady(StreetViewPanorama streetViewPanorama) {
+        streetViewPanorama.setPosition(new LatLng(-8.046004, 111.657817));
+        StreetViewPanoramaCamera camera = new StreetViewPanoramaCamera.Builder()
+                .bearing(180)
+                .build();
+        streetViewPanorama.animateTo(camera, 10000);
     }
 }
